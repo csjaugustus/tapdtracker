ec357f304ede0a1e820338bae0c7d786d89defae
Augustus Chen <csjaugustus@hotmail.com>
2022-02-10 17:03:18 +0800

    deleted get_coords.py

--- get_coords.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-print("Move mouse to desired location.")
-while True:
-	user_input = input("Press any key to get coords. 'q' to quit.")
-	if user_input == 'q':
-		break
-	else:
-		print(pyautogui.position())
