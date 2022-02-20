dc7327a266d090275407fe5ec82c04ec3c67742b
Augustus Chen <csjaugustus@hotmail.com>
2022-02-20 11:14:40 +0800

    changed how delay works. before, setting a 10sec delay would mean it will wait for 10secs before retrying, but not it will try for a maximum of 10s, breaking beforehand if it succeeds before 10s is reached

--- image_detection.py
+++ image_detection.py
@@ -4,8 +4,12 @@ import pyautogui
 from random import choice
 from win32gui import GetForegroundWindow, GetWindowRect
 import time
+import os
 
 def detect_image(template_file_name, threshold = 0.9):
+	if not os.path.exists(template_file_name):
+		raise FileNotFoundError
+
 	ss = pyautogui.screenshot()
 	img_rgb = cv2.cvtColor(np.array(ss), cv2.COLOR_RGB2BGR)
 	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
@@ -32,27 +36,44 @@ def crop_full(detected):
 	cropped = ss.crop((x_min-80, y_min-50, x_max+100, y_max+15))
 	return cropped
 
-def click_image(template_file_name, delay=None):
-	result = detect_image(template_file_name)
-	if not result:
-		if delay:
-			time.sleep(delay)
-			result = detect_image(template_file_name)
-			if not result:
-				print("No image detected.")
-				return
-		else:
-			print("No image detected.")
+def click_image(template_file_name, delay=5, double=False):
+	t = 0
+	while not detect_image(template_file_name):
+		time.sleep(0.1)
+		t += 0.1
+		if t >= delay:
+			print(f"{template_file_name} undetected.")
 			return
-	x1, x2, y1, y2 = result
+	x1, x2, y1, y2 = detect_image(template_file_name)
 	center_x = (x1 + x2)/2
 	center_y = (y1 + y2)/2
 	pyautogui.moveTo(center_x, center_y)
-	pyautogui.click()
+	if double:
+		pyautogui.doubleClick()
+	else:
+		pyautogui.click()
+
+	return (center_x, center_y)
+
+def move_to_image(template_file_name, delay=5):
+	t = 0
+	while not detect_image(template_file_name):
+		time.sleep(0.1)
+		t += 0.1
+		if t >= delay:
+			print(f"{template_file_name} undetected.")
+			return
+	x1, x2, y1, y2 = detect_image(template_file_name)
+	center_x = (x1 + x2)/2
+	center_y = (y1 + y2)/2
+	pyautogui.moveTo(center_x, center_y)
+
+	return (center_x, center_y)
 
 def main():
-	result = click_image("files\\1.png", delay=5)
+	# result = click_image("files\\1.png", delay=5)
 	# # crop_full(result).save("cropped.png")
+	move_to_image("automation\\wjcszs1.png")
 
 if __name__ == "__main__":
 	main()
